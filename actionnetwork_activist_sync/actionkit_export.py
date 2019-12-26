514ab837d76f1d55dec5dc63bd94b4e8510caaa6
Josh King <skierx@gmail.com>
2019-12-25 23:21:32 -0500

    Create new class to just handle the XLSX to Agate Table conversions
    Add test data

---
+++ actionnetwork_activist_sync/actionkit_export.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Converts ActionKit Excel exports to Agate tables"""
+
+import io
+
+import agate
+import agateexcel
+
+class ActionKitExport:
+    """ActionKitExport converts two ActionKit Excel spreadsheets.
+
+    The reason there are two is so we can calculate who is missing in
+    the new sheet and who are new members.
+
+    Args:
+        previous_file (io.IOBase): The last spreadsheet that was synced
+        current_file (io.IOBase): The newest spreadsheet to be synced
+
+    Attributes:
+        previous_file (io.IOBase): The last spreadsheet that was synced
+        current_file (io.IOBase): The newest spreadsheet to be synced
+        previous (agate.Table): The last spreadsheet after conversion
+        current (agate.Table): The newest spreadsheet after conversion
+        missing_email (agate.Table): Rows that were missing an email
+    """
+
+    def __init__(self, previous_file: io.IOBase, current_file: io.IOBase):
+        self.previous_file = previous_file
+        self.current_file = current_file
+        self.previous = None
+        self.current = None
+        self.missing_email = None
+
+    def load(self) -> None:
+        """Converts the spreadsheets to Agate tables"""
+
+        if not isinstance(self.previous_file, io.IOBase):
+            raise TypeError
+
+        if not isinstance(self.current_file, io.IOBase):
+            raise TypeError
+
+        self.previous = agate.Table.from_xlsx(self.previous_file)
+        self.current = agate.Table.from_xlsx(self.current_file)
+
+    def filter_missing_email(self) -> None:
+        """Strips out and saves rows that are missing emails"""
+        self.missing_email = self.current.where(lambda row: row['Email'] is None)
+        self.previous = self.previous.where(lambda row: row['Email'] is not None)
+        self.current = self.current.where(lambda row: row['Email'] is not None)
+
+    def get_previous_not_in_current(self) -> agate.Table:
+        """Finds rows that were missing in the current spreadsheet"""
+        return self.previous.select(['Email']) \
+            .join(self.current, 'Email', 'Email', columns=['Email']) \
+            .where(lambda row: row['Email2'] is None)
---
+++ tests/test_actionkit_export.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Tests for actionkit_export module"""
+
+import pathlib
+import unittest
+
+import agate
+
+from actionnetwork_activist_sync.actionkit_export import ActionKitExport
+
+def get_file(filename):
+    """Helper to load Excel test data"""
+    return open(pathlib.Path(__file__).parent / 'data' / filename, 'rb')
+
+
+class TestActionKitExport(unittest.TestCase):
+    """Tests the ActionKitExport class"""
+
+    def test_load_no_file_handles(self):
+        """Try to load without files"""
+        export = ActionKitExport(None, None)
+        with self.assertRaises(TypeError):
+            export.load()
+
+    def test_load_valid_files(self):
+        """Try to load with test files"""
+        export = ActionKitExport(get_file('01previous.xlsx'), get_file('01current.xlsx'))
+        export.load()
+        self.assertIsInstance(export.previous, agate.table.Table)
+        self.assertIsInstance(export.current, agate.table.Table)
+
+    def test_filter_missing_email(self):
+        """Load then filter with data that is missing emails
+
+        Previous has two rows, one missing email
+        Current has two rows, one missing email
+        """
+        export = ActionKitExport(get_file('02previous.xlsx'), get_file('02current.xlsx'))
+        export.load()
+        export.filter_missing_email()
+        self.assertEqual(len(export.missing_email.rows), 1)
+        self.assertEqual(len(export.previous.rows), 1)
+        self.assertEqual(len(export.current.rows), 1)
+
+    def test_get_previous_not_in_current(self):
+        """The full happy path"""
+        export = ActionKitExport(get_file('01previous.xlsx'), get_file('01current.xlsx'))
+        export.load()
+        export.filter_missing_email()
+        previous_not_in_current = export.get_previous_not_in_current()
+        self.assertEqual(previous_not_in_current.rows[0]['Email'], 'john.doe@example.com')
