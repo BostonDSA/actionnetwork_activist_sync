7d97fb6046c3a0e8acd83c2d0a47a20cb17e6a39
Josh King <skierx@gmail.com>
2020-01-09 22:57:46 -0500

    Refactor field_mapper

--- actionnetwork_activist_sync/field_mapper.py
+++ actionnetwork_activist_sync/field_mapper.py
@@ -1,54 +1,45 @@
 # -*- coding: utf-8 -*-
 """Logic to convert people data from ActionKit to ActionNetwork"""
 
-from typing import Dict
+from datetime import datetime
+from decimal import *
 
 class FieldMapper:
     """Map fields from ActionKit to ActionNetwork
 
     Args:
-        exported_person (Dict): Single person record from ActionKit
+        exported_person (agate.Row): Single person record from ActionKit
 
     Attributes:
-        exported_person (Dict): Single person record from ActionKit
+        exported_person (agate.Row): Single person record from ActionKit
         person_id (int): ActionNetwork ID (optional)
         overrides (dict): fields to override
         is_member (str): "1" true, "0" false (API uses strings)
     """
 
-    def __init__(self, exported_person: Dict):
+    def __init__(self, exported_person):
         self.exported_person = exported_person
         self.person_id = None
         self.overrides = {}
         self.is_member = "1"
 
-    def get_actionnetwork_person(self) -> Dict:
+    def get_actionnetwork_person(self):
         """Main conversion method"""
+
+        address = []
+        if self.exported_person.get('Address_Line_1'):
+            address.append(self.exported_person.get('Address_Line_1'))
 
         person = {
             'email': self.exported_person.get('Email'),
-            'given_name': self.exported_person.get('first_name'),
-            'family_name': self.exported_person.get('last_name'),
-            'address': [self.exported_person.get('Address_Line_1')],
-            'city': self.exported_person.get('City'),
-            'country': self.exported_person.get('Country'),
-            'postal_code': self.get_postal_code,
-            'custom_fields': {
-                'Address Line 2': self.exported_person.get('Address_Line_2'),
-                'AK_ID': self.exported_person.get('AK_ID'),
-                'BDSA Xdate': self.exported_person.get('Xdate'),
-                'Do Not Call': self.exported_person.get('Do_Not_Call'),
-                'DSA_ID': self.exported_person.get('DSA_ID'),
-                'Join Date': self.exported_person.get('Join_Date'),
-                'Mail Preference': self.exported_person.get('Mail_preference'),
-                'Middle Name': self.exported_person.get('middle_name'),
-                'Phone': self.get_phone(),
-                'is_member': self.is_member
-                # TODO: not currently in AN, but in AK
-                # 'Memb_status': self.exported_person.get('Memb_status'),
-                # 'membership_type': self.exported_person.get('membership_type'),
-                # 'monthly_status': self.exported_person.get('monthly_status')
-            }
+            'given_name': self.exported_person.get('first_name', default=''),
+            'family_name': self.exported_person.get('last_name', default=''),
+            'address': address,
+            'city': self.exported_person.get('City', default=''),
+            'state': self.exported_person.get('State', default=''),
+            'country': self.exported_person.get('Country', default=''),
+            'postal_code': self.get_postal_code(),
+            'custom_fields': self.get_custom_fields()
         }
 
         if self.person_id:
@@ -63,7 +54,7 @@ class FieldMapper:
 
         return person
 
-    def get_phone(self) -> str:
+    def get_phone(self):
         """Normalizes phone data"""
 
         # prefer mobile
@@ -82,11 +73,44 @@ class FieldMapper:
 
         return phone
 
-    def get_postal_code(self) -> str:
+    def get_postal_code(self):
         """Normalizes postal code data"""
 
-        postal_code = self.exported_person.get('Zip')
+        postal_code = self.exported_person.get('Zip', default='')
         if postal_code and len(postal_code) < 5 and postal_code.isnumeric():
             postal_code = f'{postal_code:0>5}'
 
         return postal_code
+
+    def get_custom_fields(self):
+        """Formats custom fields"""
+
+        custom_fields = {
+            'Address Line 2': self.exported_person.get('Address_Line_2'),
+            'AK_ID': self.exported_person.get('AK_ID'),
+            'BDSA Xdate': self.exported_person.get('Xdate'),
+            'Do Not Call': self.exported_person.get('Do_Not_Call'),
+            'DSA_ID': self.exported_person.get('DSA_ID'),
+            'Join Date': self.exported_person.get('Join_Date'),
+            'Mail Preference': self.exported_person.get('Mail_preference'),
+            'Middle Name': self.exported_person.get('middle_name'),
+            'Phone': self.get_phone(),
+            'is_member': self.is_member
+            # TODO: not currently in AN, but in AK
+            # 'Memb_status': self.exported_person.get('Memb_status'),
+            # 'membership_type': self.exported_person.get('membership_type'),
+            # 'monthly_status': self.exported_person.get('monthly_status')
+        }
+
+        # filter None
+        custom_fields = {k:v for k,v in custom_fields.items() if v is not None}
+
+        for k,v in custom_fields.items():
+            if isinstance(v, Decimal):
+                custom_fields[k] = str(v)
+            elif isinstance(v, datetime):
+                custom_fields[k] = str(v)
+            elif isinstance(v, bool):
+                custom_fields[k] = 'True' if v else 'False'
+
+        return custom_fields
--- tests/test_field_mapper.py
+++ tests/test_field_mapper.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """Test field conversions"""
 
+from datetime import datetime
+from decimal import *
+from pprint import pprint
 import unittest
 
+from agate import Row
 from ddt import ddt, data, unpack
 
 from actionnetwork_activist_sync.field_mapper import FieldMapper
@@ -21,7 +25,7 @@ class TestFieldMapper(unittest.TestCase):
     def test_get_phone(self, raw, expected):
         """Test known phone formats"""
 
-        field_mapper = FieldMapper({'Mobile_Phone': raw})
+        field_mapper = FieldMapper(Row([raw], ['Mobile_Phone']))
         actual = field_mapper.get_phone()
         self.assertEqual(expected, actual)
 
@@ -34,18 +38,29 @@ class TestFieldMapper(unittest.TestCase):
     def test_get_postal_code(self, raw, expected):
         """Test known zip formats"""
 
-        field_mapper = FieldMapper({'Zip': raw})
+        field_mapper = FieldMapper(Row([raw], ['Zip']))
         actual = field_mapper.get_postal_code()
         self.assertEqual(expected, actual)
 
     def test_overrides_basic_field(self):
-        field_mapper = FieldMapper({'given_name': 'Test'})
+        field_mapper = FieldMapper(Row(['Test'], ['given_name']))
         field_mapper.overrides = {'given_name': 'NewName'}
         person = field_mapper.get_actionnetwork_person()
         self.assertEqual('NewName', person['given_name'])
 
     def test_overrides_custom_field(self):
-        field_mapper = FieldMapper({'Phone': '6175555555'})
+        field_mapper = FieldMapper(Row(['6175555555'], ['Mobile_Phone']))
         field_mapper.overrides = {'Phone': '6176666666'}
         person = field_mapper.get_actionnetwork_person()
         self.assertEqual('6176666666', person['custom_fields']['Phone'])
+
+    @data(
+        ('Address_Line_2', 'Apt 123', 'Address Line 2', 'Apt 123'),
+        ('AK_ID', Decimal(1), 'AK_ID', '1'),
+        ('Join_Date', datetime(2020, 1, 9), 'Join Date', '2020-01-09 00:00:00')
+    )
+    @unpack
+    def test_get_custom_fields(self, key, value, expected_key, expected_value):
+        field_mapper = FieldMapper(Row([value], [key]))
+        custom_fields = field_mapper.get_custom_fields()
+        self.assertEqual(custom_fields[expected_key], expected_value)
