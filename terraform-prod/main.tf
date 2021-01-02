d1d06b8b860ce992e94dd5f131d137543ac68d9f
Josh King <skierx@gmail.com>
2021-01-01 21:47:36 -0500

    Lambda terraform updates * Rename incoming to ingester for consistancy * Add management of logging * Add processor lambda * Add dynamodb permission

--- terraform-prod/main.tf
+++ terraform-prod/main.tf
@@ -33,7 +33,7 @@ resource "aws_ses_active_receipt_rule_set" "default" {
 }
 
 resource "aws_ses_receipt_rule" "an-sync-ses-rule" {
-  name          = "${module.shared.project}_incoming"
+  name          = "${module.shared.project}_ingester"
   rule_set_name = aws_ses_receipt_rule_set.default.rule_set_name
   recipients    = [local.email]
   enabled       = true
@@ -44,7 +44,7 @@ resource "aws_ses_receipt_rule" "an-sync-ses-rule" {
   }
 }
 
-# S3 Bucket to deposit incoming emails into
+# S3 Bucket to deposit ingester emails into
 
 data "aws_iam_policy_document" "an-sync-bucket-policy" {
   statement {
@@ -88,14 +88,14 @@ resource "aws_s3_bucket_notification" "an-sync-bucket-notification" {
   bucket = aws_s3_bucket.an-sync-bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.an-sync-incoming-lambda.arn
+    lambda_function_arn = aws_lambda_function.an-sync-ingester-lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
 
-  depends_on = [aws_lambda_permission.an-sync-incoming-lambda-permission]
+  depends_on = [aws_lambda_permission.an-sync-ingester-lambda-permission]
 }
 
-# Lambda to process incoming messages
+# Lambda to process ingester messages
 
 data "aws_iam_policy_document" "an-sync-lambda-policy-assume" {
   statement {
@@ -123,36 +123,76 @@ data "aws_iam_policy_document" "an-sync-lambda-policy-attach" {
     ]
     resources = [format("%s/*", aws_s3_bucket.an-sync-bucket.arn)]
   }
+  statement {
+    actions = [
+      "dynamodb:*"
+    ]
+    resources = [module.shared.db-table.arn, module.shared.db-table.stream_arn]
+  }
 }
 
 resource "aws_iam_policy" "an-sync-lambda-policy-attach" {
   policy = data.aws_iam_policy_document.an-sync-lambda-policy-attach.json
 }
 
-resource "aws_iam_role" "an-sync-incoming-lambda-role" {
-  name               = "an-sync-incoming-lambda-role"
+resource "aws_iam_role" "an-sync-lambda-role" {
+  name               = "an-sync-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.an-sync-lambda-policy-assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "an-sync-lambda-policy-attach" {
-  role       = aws_iam_role.an-sync-incoming-lambda-role.name
+  role       = aws_iam_role.an-sync-lambda-role.name
   policy_arn = aws_iam_policy.an-sync-lambda-policy-attach.arn
 }
 
-resource "aws_lambda_permission" "an-sync-incoming-lambda-permission" {
-  # Let the S3 bucket trigger the lambda
+resource "aws_lambda_permission" "an-sync-ingester-lambda-permission" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.an-sync-incoming-lambda.function_name
+  function_name = aws_lambda_function.an-sync-ingester-lambda.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.an-sync-bucket.arn
 }
 
-resource "aws_lambda_function" "an-sync-incoming-lambda" {
+resource "aws_lambda_function" "an-sync-ingester-lambda" {
+  description      = "Action Network Sync S3 Ingester (Step 1)"
   filename         = "../dist/sync.zip"
   source_code_hash = filebase64sha256("../dist/sync.zip")
-  function_name    = "an-sync-incoming-lambda"
-  role             = aws_iam_role.an-sync-incoming-lambda-role.arn
+  function_name    = "an-sync-ingester-lambda"
+  role             = aws_iam_role.an-sync-lambda-role.arn
   handler          = "lambda_ingester.lambda_handler"
   runtime          = "python3.7"
+}
+
+resource "aws_cloudwatch_log_group" "an-sync-ingester-lambda" {
+  name              = "/aws/lambda/${aws_lambda_function.an-sync-ingester-lambda.function_name}"
+  retention_in_days = 60
+}
+
+# Lambda to process the DynamoDB items
+
+resource "aws_lambda_event_source_mapping" "processor" {
+  event_source_arn  = module.shared.db-table.stream_arn
+  function_name     = aws_lambda_function.an-sync-processor-lambda.arn
+  starting_position = "LATEST"
+}
+
+resource "aws_lambda_function" "an-sync-processor-lambda" {
+  description      = "Action Network Sync DynamoDB Processor (Step 2)"
+  filename         = "../dist/sync.zip"
+  source_code_hash = filebase64sha256("../dist/sync.zip")
+  function_name    = "an-sync-processor-lambda"
+  role             = aws_iam_role.an-sync-lambda-role.arn
+  handler          = "lambda_processor.lambda_handler"
+  runtime          = "python3.7"
+
+  environment {
+    variables = {
+      DRY_RUN = "1"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "an-sync-processor-lambda" {
+  name              = "/aws/lambda/${aws_lambda_function.an-sync-processor-lambda.function_name}"
+  retention_in_days = 60
 }
