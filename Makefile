e204b31d03d92b95ab99e06c123839d6e9647135
Josh King <skierx@gmail.com>
2021-01-12 21:07:17 -0500

    Dev updates:   localstack doesn't use individual endpoints anymore   need to terraform init on fresh machines

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 # Developer Ingest
-local-ingest: local-init-ingest local-upload-sample
+local-ingest: local-init-ingest local-upload-sample local-ingest-run
 
 local-init-ingest:
 	-pipenv shell
 	docker-compose up -d
-	terraform -chdir=terraform-dev apply-auto-approve
+	terraform -chdir=terraform-dev init
+	terraform -chdir=terraform-dev apply -auto-approve
 
 local-upload-sample:
 	awslocal s3 cp sample.email s3://actionnetworkactivistsync.bostondsa.net/sample.email
 
-local-ingest:
+local-ingest-run:
 	pipenv run python-lambda-local -f lambda_handler lambda_ingester.py lambda_ingester_event.json
 
 # Developer Processor
--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -10,25 +10,8 @@ provider "aws" {
 
 
   endpoints {
-    # apigateway     = "http://localhost:4567"
-    # cloudformation = "http://localhost:4581"
-    # cloudwatch     = "http://localhost:4582"
-    dynamodb = "http://localhost:4569"
-    # es             = "http://localhost:4578"
-    # firehose       = "http://localhost:4573"
-    # iam            = "http://localhost:4593"
-    # kinesis        = "http://localhost:4568"
-    # lambda         = "http://localhost:4574"
-    # route53        = "http://localhost:4580"
-    # redshift       = "http://localhost:4577"
-    s3 = "http://localhost:4566"
-    # secretsmanager = "http://localhost:4584"
-    # ses            = "http://localhost:4579"
-    # sns            = "http://localhost:4575"
-    sqs = "http://localhost:4576"
-    # ssm            = "http://localhost:4583"
-    # stepfunctions  = "http://localhost:4585"
-    # sts            = "http://localhost:4592"
+    dynamodb = "http://localhost:4566"
+    s3       = "http://localhost:4566"
   }
 }
 
