02cc92d134a206498d6469f8bdc424e5c5c5ced9
Josh King <skierx@gmail.com>
2021-01-30 20:46:12 -0500

    Add the lambda for lapsed members. This will use the dynamo state to figure out who wasn't in the most recent file.

---
+++ lambda_lapsed.py
@@ -0,0 +1,88 @@
+"""
+This lambda compares new batches to previous batches to detect
+which records are missing from the new one. These indicate that
+a membership has lapsed.
+"""
+
+import datetime
+import json
+import os
+
+from agate.rows import Row
+import boto3
+
+from actionnetwork_activist_sync.actionnetwork import ActionNetwork
+from actionnetwork_activist_sync.logging import get_logger
+from actionnetwork_activist_sync.field_mapper import FieldMapper
+from actionnetwork_activist_sync.state_model import State
+
+logger = get_logger('lambda_lapsed')
+
+dry_run = os.environ.get('DRY_RUN') != '0'
+dynamodb_client = boto3.client('dynamodb')
+secrets_client = boto3.client('secretsmanager')
+
+api_key = os.environ['ACTIONNETWORK_API_KEY']
+if api_key.startswith('arn'):
+    secret = secrets_client.get_secret_value(SecretId=api_key)
+    secret_dict = json.loads(secret['SecretString'])
+    api_key = secret_dict['ACTIONNETWORK_API_KEY']
+    logger.debug('Using API key from Secrets Manager')
+else:
+    logger.debug('Using API key from Env')
+
+if os.environ.get('ENVIRONMENT') == 'local':
+    import localstack_client.session
+    dynamodb_client = localstack_client.session.Session().client('dynamodb')
+
+cur_batch = datetime.date.today().strftime('%Y%U')
+last_week = datetime.date.today() - datetime.timedelta(weeks=1)
+prev_batch = last_week.strftime('%Y%U')
+
+def lambda_handler(event, context):
+    """
+    Detect changes
+    Flip is_member
+    """
+
+    removed = 0
+    cur_count = State.count(hash_key=cur_batch)
+    prev_count = State.count(hash_key=prev_batch)
+
+    cur_items = State.query(hash_key=cur_batch)
+    logger.info(
+        'Loaded current items.',
+        extra={'cur_batch': cur_batch, 'num_items': cur_count})
+
+    prev_items = State.query(hash_key=prev_batch)
+    logger.info(
+        'Loaded previous items.',
+        extra={'prev_batch': prev_batch, 'num_items': prev_count})
+
+    cur_emails = [c.email for c in cur_items]
+    prev_emails = [p.email for p in prev_items]
+
+    logger.info(
+        'Checking previous email list against current',
+        extra={
+            'cur_email_count': len(cur_emails),
+            'prev_email_count': len(prev_emails)
+        }
+    )
+
+    action_network = get_actionnetwork(api_key)
+
+    for prev_email in prev_emails:
+        if prev_email not in cur_emails:
+            logger.info(
+                'Turing is_member off for lapsed member',
+                extra={'email': prev_email}
+            )
+            if not dry_run:
+                action_network.remove_member_by_email(prev_email)
+            removed += 1
+
+    return (removed, cur_count, prev_count)
+
+def get_actionnetwork(api_key):
+    return ActionNetwork(api_key)
---
+++ tests/test_lambda_lapsed.py
@@ -0,0 +1,129 @@
+import json
+import importlib
+import os
+import unittest
+from unittest.mock import Mock
+
+from moto import mock_dynamodb2
+import boto3
+from lambda_local.context import Context
+
+os.environ['ENVIRONMENT'] = 'TEST'
+os.environ['LOG_LEVEL'] = 'CRITICAL'
+os.environ['DSA_KEY'] = 'TESTKEY'
+
+class TestLapsed(unittest.TestCase):
+
+    @mock_dynamodb2
+    def test_in_both_is_noop(self):
+        import lambda_lapsed
+        from actionnetwork_activist_sync.actionnetwork import ActionNetwork
+        from actionnetwork_activist_sync.state_model import State
+
+        State.create_table(billing_mode='PAY_PER_REQUEST')
+
+        j_karl = json.dumps({
+                'Email': 'kmarx@marxists.org',
+                'firstname': 'Karl',
+                'lastname': 'Marx'
+        })
+
+        cur_karl = State(
+            lambda_lapsed.cur_batch,
+            'kmarx@marxists.org',
+            raw=j_karl,
+            status=State.UNPROCESSED
+        )
+        cur_karl.save()
+
+        prev_karl = State(
+            lambda_lapsed.prev_batch,
+            'kmarx@marxists.org',
+            raw=j_karl,
+            status=State.UNPROCESSED
+        )
+        prev_karl.save()
+
+        mock_an = Mock(ActionNetwork)
+        lambda_lapsed.get_actionnetwork = lambda a: mock_an
+
+        (rem, cur, prev) = lambda_lapsed.lambda_handler({}, Context(5))
+
+        self.assertEqual(rem, 0)
+        self.assertEqual(cur, 1)
+        self.assertEqual(prev, 1)
+
+    @mock_dynamodb2
+    def test_in_cur_but_not_in_prev_is_noop(self):
+        import lambda_lapsed
+        from actionnetwork_activist_sync.actionnetwork import ActionNetwork
+        from actionnetwork_activist_sync.state_model import State
+
+        importlib.reload(lambda_lapsed)
+        lambda_lapsed.get_actionnetwork = lambda a: None
+
+        State.create_table(billing_mode='PAY_PER_REQUEST')
+
+        j_karl = json.dumps({
+                'Email': 'kmarx@marxists.org',
+                'firstname': 'Karl',
+                'lastname': 'Marx'
+        })
+
+        cur_karl = State(
+            lambda_lapsed.cur_batch,
+            'kmarx@marxists.org',
+            raw=j_karl,
+            status=State.UNPROCESSED
+        )
+        cur_karl.save()
+
+        mock_an = Mock(ActionNetwork)
+        lambda_lapsed.get_actionnetwork = lambda a: mock_an
+
+        (rem, cur, prev) = lambda_lapsed.lambda_handler({}, Context(5))
+
+        self.assertEqual(rem, 0)
+        self.assertEqual(cur, 1)
+        self.assertEqual(prev, 0)
+
+    @mock_dynamodb2
+    def test_not_in_cur_but_in_prev_gets_removed(self):
+        import lambda_lapsed
+        from actionnetwork_activist_sync.actionnetwork import ActionNetwork
+        from actionnetwork_activist_sync.state_model import State
+
+        # this lets us make sure the mock gets called
+        os.environ['DRY_RUN'] = '0'
+        importlib.reload(lambda_lapsed)
+
+        State.create_table(billing_mode='PAY_PER_REQUEST')
+
+        j_karl = json.dumps({
+                'Email': 'kmarx@marxists.org',
+                'firstname': 'Karl',
+                'lastname': 'Marx'
+        })
+
+        prev_karl = State(
+            lambda_lapsed.prev_batch,
+            'kmarx@marxists.org',
+            raw=j_karl,
+            status=State.UNPROCESSED
+        )
+        prev_karl.save()
+
+        mock_an = Mock(ActionNetwork)
+        mock_an.remove_member_by_email = Mock()
+        lambda_lapsed.get_actionnetwork = lambda a: mock_an
+
+        (rem, cur, prev) = lambda_lapsed.lambda_handler({}, Context(5))
+
+        mock_an.remove_member_by_email.assert_called_once_with(
+            'kmarx@marxists.org'
+        )
+        self.assertEqual(rem, 1)
+        self.assertEqual(cur, 0)
+        self.assertEqual(prev, 1)
+
+        del os.environ['DRY_RUN']
