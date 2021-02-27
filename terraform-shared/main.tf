8682c8f2366c46f5822cc52a8aa9a0361e453a7c
Josh King <skierx@gmail.com>
2021-02-26 21:30:09 -0500

    Flip the Dynamo table to match the code

--- terraform-shared/main.tf
+++ terraform-shared/main.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "an-sync" {
   name             = var.project
   billing_mode     = "PAY_PER_REQUEST"
-  hash_key         = "email"
-  range_key        = "batch"
+  hash_key         = "batch"
+  range_key        = "email"
   stream_enabled   = true
   stream_view_type = "KEYS_ONLY"
 
