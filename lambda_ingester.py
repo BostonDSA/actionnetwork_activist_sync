05a88345ff6c8a16d027da3b058e6fd87e114d86
Josh King <skierx@gmail.com>
2021-07-08 22:40:04 -0400

    Change lambda to accept new input format

--- lambda_ingester.py
+++ lambda_ingester.py
@@ -54,103 +54,102 @@ def lambda_handler(event, context):
     """
 
     count = 0
-    for record in event['Records']:
 
-        # Fetch email from bucket
-        bucket = record['s3']['bucket']['name']
-        key = unquote_plus(record['s3']['object']['key'])
-        tmpkey = key.replace('/', '')
-        download_path = f'/tmp/{uuid.uuid4()}{tmpkey}'
-        logger.info(
-            'Downloading file from bucket',
-            extra={"bucket": bucket, "key": key, "download_path": download_path})
-        s3_client.download_file(bucket, key, download_path)
+    # Fetch email from bucket
+    bucket = event['detail']['requestParameters']['bucketName']
+    key = unquote_plus(event['detail']['requestParameters']['key'])
+    tmpkey = key.replace('/', '')
+    download_path = f'/tmp/{uuid.uuid4()}{tmpkey}'
+    logger.info(
+        'Downloading file from bucket',
+        extra={"bucket": bucket, "key": key, "download_path": download_path})
+    s3_client.download_file(bucket, key, download_path)
 
-        with open(download_path) as email_file:
-            # The full email gets deposited in the S3 bucket
-            msg = email.message_from_file(email_file, policy=email.policy.default)
+    with open(download_path) as email_file:
+        # The full email gets deposited in the S3 bucket
+        msg = email.message_from_file(email_file, policy=email.policy.default)
 
-            if dsa_key != msg.get('DsaKey'):
-                raise ValueError('DSA Key not found in email header, aborting.')
+        if dsa_key != msg.get('DsaKey'):
+            raise ValueError('DSA Key not found in email header, aborting.')
 
-            # ActionKit mails the report as an attached ZIP file
-            attach = next(msg.iter_attachments())
+        # ActionKit mails the report as an attached ZIP file
+        attach = next(msg.iter_attachments())
 
-            if not attach.get_content_type() in ['application/zip', 'application/x-zip-compressed']:
-                logger.error(
-                    'Attachment is not ZIP',
-                    extra={'content_type': attach.get_content_type()})
-                raise ValueError('Attachment is not ZIP')
+        if not attach.get_content_type() in ['application/zip', 'application/x-zip-compressed']:
+            logger.error(
+                'Attachment is not ZIP',
+                extra={'content_type': attach.get_content_type()})
+            raise ValueError('Attachment is not ZIP')
 
-            zip_data = io.BytesIO(attach.get_content())
+        zip_data = io.BytesIO(attach.get_content())
 
-            with zipfile.ZipFile(zip_data) as zip:
-                names = zip.namelist()
-                if len(names) != 1:
-                    raise ValueError('ZIP archive has wrong number of files')
+        with zipfile.ZipFile(zip_data) as zip:
+            names = zip.namelist()
+            if len(names) != 1:
+                raise ValueError('ZIP archive has wrong number of files')
 
-                if not names[0].endswith('.csv'):
-                    raise ValueError('ZIP archive is missing CSV file')
+            if not names[0].endswith('.csv'):
+                raise ValueError('ZIP archive is missing CSV file')
 
-                csv_lines = io.StringIO(zip.read(names[0]).decode('utf-8'))
+            csv_lines = io.StringIO(zip.read(names[0]).decode('utf-8'))
 
 
-            for row in csv.DictReader(csv_lines):
-                d_row = dict(row)
+        for row in csv.DictReader(csv_lines):
+            d_row = dict(row)
 
-                if 'Email' not in d_row or not d_row['Email']:
-                    # We can't continue processing without an email
-                    continue
+            if 'Email' not in d_row or not d_row['Email']:
+                # We can't continue processing without an email
+                continue
 
-                state = State(
-                    batch,
-                    d_row['Email'],
-                    raw=json.dumps(d_row),
-                    status=State.UNPROCESSED
-                )
-                state.save()
+            state = State(
+                batch,
+                d_row['Email'],
+                raw=json.dumps(d_row),
+                status=State.UNPROCESSED
+            )
+            state.save()
 
-                count += 1
+            count += 1
 
-            # TODO: move to later in the step function
+        # TODO: move to later in the step function
 
-            topic = os.environ.get('SLACK_TOPIC_ARN')
-            chan = os.environ.get('SLACK_CHANNEL')
-            if False and not os.environ.get('ENVIRONMENT') == 'local' and topic and chan:
-                sns_client.publish(
-                    TopicArn=topic,
-                    Message=json.dumps({
-                        'channel': chan,
-                        'text': 'New member data has arrived from national',
-                        'attachments': [
-                            {
-                                'color': 'b71c1c',
-                                'fallback': 'New member data has arrived from national',
-                                'fields': [
-                                    {
-                                        'title': 'Number of rows',
-                                        'value': count,
-                                        'short': True
-                                    }
-                                ],
-                                'footer': '<https://github.com/BostonDSA/actionnetwork_activist_sync|BostonDSA/actionnetwork_activist_sync>',
-                                'footer_icon': 'https://github.com/favicon.ico',
+        topic = os.environ.get('SLACK_TOPIC_ARN')
+        chan = os.environ.get('SLACK_CHANNEL')
+        if False and not os.environ.get('ENVIRONMENT') == 'local' and topic and chan:
+            sns_client.publish(
+                TopicArn=topic,
+                Message=json.dumps({
+                    'channel': chan,
+                    'text': 'New member data has arrived from national',
+                    'attachments': [
+                        {
+                            'color': 'b71c1c',
+                            'fallback': 'New member data has arrived from national',
+                            'fields': [
+                                {
+                                    'title': 'Number of rows',
+                                    'value': count,
+                                    'short': True
+                                }
+                            ],
+                            'footer': '<https://github.com/BostonDSA/actionnetwork_activist_sync|BostonDSA/actionnetwork_activist_sync>',
+                            'footer_icon': 'https://github.com/favicon.ico',
 
-                            }
-                        ]
-                    }),
-                    MessageAttributes={
-                        'id': {
-                            'DataType': 'String',
-                            'StringValue': 'postMessage'
-                        },
-                        'type': {
-                            'DataType': 'String',
-                            'StringValue': 'chat'
                         }
+                    ]
+                }),
+                MessageAttributes={
+                    'id': {
+                        'DataType': 'String',
+                        'StringValue': 'postMessage'
+                    },
+                    'type': {
+                        'DataType': 'String',
+                        'StringValue': 'chat'
                     }
-                )
-            logger.info('Finished processing CSV.', extra={'num_rows': count})
+                }
+            )
+        logger.info('Finished processing CSV.', extra={'num_rows': count})
 
     return {
         'batch': batch,
